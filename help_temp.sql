50e01e669f4e8b2654dd0ad17aefc7ed7049f4fb
Jagadees <jagatheeshthangaraj@gmail.com>
2020-08-25 13:44:52 +0530

    Add some queries.Nothing big

--- help_temp.sql
+++ help_temp.sql
@@ -4,8 +4,12 @@ truncate table follow_requests;
 truncate table follow_requests_accepted;
 end transaction;
 
-
+a 8cc9d8b6f68bfbd5eb59a1e056b8a7de9bda138c 121
+b c59f294a176cc9ba303a05d7e598d33492e87a57 122
+c 4c7b68e11ee73d5d4067fd9ce1492d05724d205c 123
+http://127.0.0.1:8000/
 select * from followers;
 select * from follow_requests;
 select * from follow_requests_accepted;
+
 
--- tables.sql
+++ tables.sql
@@ -1,4 +1,11 @@
+#Readmeeeeeeeeeeeeeeeee: Do not commit inside a procedure while using pscopg2
+
+-- Username availability check
+-- If it returns 0,username is available.Case sensitive
+select count(username) from auth_user where username='Al';
+
 -- Follow Requests
+-- ToDo==>primary key (req_sender_id,req_receiver_id)
 create table follow_requests (req_sender_id integer,req_receiver_id integer,req_time timestamptz);
 insert into follow_requests(req_sender_id,req_receiver_id,req_time) values(120,122,current_timestamp);
 -- ToDo while returning,also return whether_i_am_following_the_user_who_sent_me_the_follow_request (It's available as a separeat function.I wrote it)
@@ -9,29 +16,29 @@ select req_sender_id from follow_requests where req_sender_id=1 order by req_tim
 -- Follow Request accepted by receiver / deleted by sender /deleted by receiver
 delete from follow_requests where req_sender_id=1 and req_receiver_id=2;
 
--- transactions:
-	-- Dont run separate queries.Coz one may get done.another may throw exceyion.half-done is dangerous
-	-- accept follow request
-		begin;
-		-- delete the follow request
-		delete from follow_requests where req_sender_id=120 and req_receiver_id=122;
-		-- add to accepted requests
-		insert into follow_requests_accepted(req_acceptor_id,req_acceptee_id,accept_time) values(120,122,current_timestamp);
-		--add to my followers
-		insert into followers(follower_id,followee_id) values(120,122);
-		-- it means commit.You can use it too.
-		end transaction;
 
--- notifications:Follow Requests Accepted
+-- TRANSACTION == -- accept follow request
+begin;
+-- delete the follow request
+delete from follow_requests where req_sender_id=120 and req_receiver_id=122;
+-- add to accepted requests
+insert into follow_requests_accepted(req_acceptor_id,req_acceptee_id,accept_time) values(120,122,current_timestamp);
+--add to my followers
+insert into followers(follower_id,followee_id) values(120,122);
+-- it means commit.You can use it too.
+end transaction;
+
+-- Follow Requests Accepted
 create table follow_requests_accepted(accept_id SERIAL PRIMARY KEY, req_acceptor_id integer,req_acceptee_id integer,accept_time timestamptz);
 insert into follow_requests_accepted(req_acceptor_id,req_acceptee_id,accept_time) values(1,2,current_timestamp);
 -- first request:Get new accepted_follow_requests for a guy by timestamp desc.Only new should be "Indicated".Client should store the latest accept_id it received to detect collisions.And it should stop sending requests once collision happens.
-select req_acceptor_id from follow_requests_accepted where req_acceptee_id=2 order by accept_time desc limit 5;
+select req_acceptor_id,accept_id from follow_requests_accepted where req_acceptee_id=121 order by accept_time desc limit 5;
 -- Subsequent requests use accept_id received from client to find "Older" notifs.
-select req_acceptor_id from follow_requests_accepted where req_acceptee_id=2 and accept_id<10 order by accept_time desc limit 5;
+select req_acceptor_id,accept_id from follow_requests_accepted where req_acceptee_id=121 and accept_id<7 order by accept_time desc limit 5;
 
 
 -- Followers
+-- ToDo==> pkey(follower_id,followee_id) to avoid duplicates
 create table followers(follower_id integer,followee_id integer);
 insert into followers(follower_id,followee_id) values(1,2);
 	-- My followers
@@ -44,7 +51,7 @@ delete from followers where follower_id=1 and followee_id=2;
 
 -- Follow status
 	-- Status codes: 1.Not Following 2.Request sent 3.Following
-create function return_follow_status(user_A_id integer,user_B_id integer)
+create function follow_status(user_A_id integer,user_B_id integer)
   returns integer
 as
 $$
@@ -67,21 +74,38 @@ end;
 $$
 language plpgsql;
 
--- Find nearby people
-	select user_id from user_last_location where ST_DWithin(last_location, ST_MakePoint(2,3)::geography, 5000) limit 20;
+select "follow_status"(123,121);
 
--- Search nearby people
-	select user_id from user_last_location where ST_DWithin(last_location, ST_MakePoint(2,3)::geography, 5000) and user_id=(select id from auth_user where username like '%search_query_here%')
+-- Find nearby people I dont follow
+-- Warn:Query returns user_id too.We remove it in python.Why NOT IN? != should be used only when subquery returns one row or result
+select user_id from user_last_location where ST_DWithin(last_location,(select last_location from user_last_location where user_id=123),5000) and user_id  NOT IN(select followee_id from followers where follower_id=123) order by last_time desc limit 10;
 
 -- Last location of user
-create table user_last_location(user_id integer,last_location geography);
+create table user_last_location(user_id integer,last_location geography,last_time timestamptz);
 	-- Check if this is the first insert.If result is 0,insert.Else update
 select count(user_id) from user_last_location where user_id=1;
-	-- First entry:INSERT
-insert into user_last_location(user_id,last_location) values (1,ST_MakePoint(10.1,11.2))
-	-- Not first entry:UPDATE
-update user_last_location SET last_location=ST_MakePoint(10.1,11.2) where user_id=1;
 
+-- Update user location
+-- do not commit inside a procedure while using pycopg2
+CREATE OR REPLACE PROCEDURE update_user_location(
+	user_id_ integer,longitude_ float(3),latitude_ float(3)
+)
+LANGUAGE 'plpgsql'
+AS $BODY$
+declare
+	useless_ integer; 
+BEGIN
+	if exists (select user_id from user_last_location where user_id=user_id_) then
+		update user_last_location SET last_location=ST_MakePoint(longitude_,latitude_), last_time=current_timestamp where user_id=1;
+	else
+		insert into user_last_location(user_id,last_location,last_time) values (user_id_,ST_MakePoint(longitude_,latitude_),current_timestamp);
+	end if;
+END;
+$BODY$;
+
+call update_user_location(121,10.1,11.2);
+
+--------DONE Aug23---------
 
 -- Public posts
 
@@ -105,7 +129,7 @@ BEGIN
 	new_post_id:=new_post_id+1;
 	raise notice ' New public_post_id for user_id % is %',user_id_,new_post_id;
 	insert into public_posts(user_id,public_post_id,public_post_location,public_post_time,views,likes,dislikes,deleted) values(user_id_,new_post_id,ST_MakePoint(longitude_,latitude_),current_timestamp,0,0,0,false);
-	commit;
+	-- Do not commit inside a procedure while psycopg.. Use connection.commit()
 	raise notice 'public post created successfully %',user_id_;
 END;
 $BODY$;
@@ -138,7 +162,7 @@ BEGIN
 		raise exception 'PG:Wrong action number from client';
 	end if;
 	update public_posts set views=views_,likes=likes_,dislikes=dislikes_ where user_id=user_id_ and public_post_id=public_post_id_;
-	commit;
+	-- Do not commit inside a procedure while psycopg.. Use connection.commit()
 	raise notice 'PG:public action_ % done successfully',action_;
 END;
 $BODY$;
@@ -168,8 +192,9 @@ update public_posts set deleted=true where user_id=1 and public_post_id=1;
 
 
 -- Private posts
-	-- Table
-	create table private_posts(user_id integer,private_post_id integer,private_post_time timestamptz,views integer,likes integer,deleted boolean);
+-- Table
+create table private_posts(user_id integer,private_post_id integer,private_post_time timestamptz,views integer,likes integer,deleted boolean);
+
 
 -- make private post
 CREATE OR REPLACE PROCEDURE make_private_post(
@@ -185,7 +210,7 @@ BEGIN
 	new_post_id:=new_post_id+1;
 	raise notice ' New private_post_id for user_id % is %',user_id_,new_post_id;
 	insert into private_posts(user_id,private_post_id,private_post_time,views,likes,deleted) values(user_id_,new_post_id,current_timestamp,0,0,false);
-	commit;
+	-- Do not commit inside a procedure while psycopg.. Use connection.commit()
 	raise notice 'Private post created successfully by %',user_id_;
 END;
 $BODY$;
@@ -212,7 +237,7 @@ BEGIN
 		views_=views_+1;
 	end if;
 	update private_posts set views=views_,likes=likes_ where user_id=user_id_ and private_post_id=private_post_id_;
-	commit;
+	-- Do not commit inside a procedure while psycopg.. Use connection.commit()
 	raise notice 'PG:private action_ % done successfully',action_;
 	END;
 $BODY$;
@@ -222,6 +247,8 @@ $BODY$;
 
 -- delete private post
 	update private_posts set deleted=true where user_id=1 and private_post_id=1;
+
+
 
 -- private feed
 	-- First request
@@ -233,11 +260,6 @@ $BODY$;
 	-- Are there "new" private posts available? IF result is non-zero,AVAILABLE.
 	-- Receive "TOP" post id,return count(content) newer than that.If user wish to see the enw content,he can hit refresh.
 	select count(private_post_id) from private_posts where user_id=(select followee_id from followers where follower_id=1) and private_post_time>(select private_post_time from private_posts where user_id=1 and private_post_id=2);
--- Users table
-
--- Username availability check
--- If it returns 0,username is available.Case sensitive
-select count(username) from auth_user where username='Al';
 
 -- Change username 
 update auth_user set username='Jxxxxxxx' where id=82;
@@ -245,3 +267,6 @@ update auth_user set username='Jxxxxxxx' where id=82;
 -- Get user name
 select username from auth_user where user_id
 
+
+-- Search nearby people
+select user_id from user_last_location where ST_DWithin(last_location, ST_MakePoint(2,3)::geography, 5000) and user_id=(select id from auth_user where username like '%search_query_here%')
